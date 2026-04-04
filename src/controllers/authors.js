4542336f03a555b5602fa914c33aad4a31cbb600
Fabian <skualo_fv@msn.com>
2026-04-04 15:46:05 -0300

    Authors doc imrpoved

--- src/controllers/authors.js
+++ src/controllers/authors.js
@@ -2,9 +2,9 @@ const db = require('../models');
 const Authors = db.authors;
 
 exports.findAll = async (req, res) => {
-    /* #swagger.tags = ['Authors']
+  /* #swagger.tags = ['Authors']
      #swagger.summary = 'Get all library Authors'
-     #swagger.description = 'Retrieves a list of all Author books registered in the database.'
+     #swagger.description = 'Retrieves a list of all authors registered in the database.'
      #swagger.security = [{ "github_auth": [] }]
 
      #swagger.responses[200] = { 
@@ -39,8 +39,38 @@ exports.findAll = async (req, res) => {
 };
 
 exports.findOne = async (req, res) => {
-   /* #swagger.tags = ['Authors']
-   */
+  /* #swagger.tags = ['Authors']
+     #swagger.summary = 'Get a single author by ID'
+     #swagger.description = 'Retrieves a specific author from the library collection using their unique MongoDB ObjectId.'
+     #swagger.security = [{ "github_auth": [] }]
+
+     #swagger.parameters['author_id'] = {
+        in: 'path',
+        description: 'The unique ID of the author',
+        required: true,
+        type: 'string'
+     }
+
+     #swagger.responses[200] = { 
+        description: 'Author found and retrieved successfully',
+        schema: { 
+            $firstName: 'Samuel',
+            $lastName: 'Clemens',
+            $pseudonym: 'Mark Twain',
+            birthDate: '1835-11-30',
+            deathDate: '1910-04-21',
+            nationality: 'American',
+            mainGenre: 'Fiction',
+            books: ['The Great Gatsby', 'To Kill a Mockingbird']
+        }
+     }
+     #swagger.responses[404] = { 
+        description: 'Author not found in the library collection' 
+     }
+     #swagger.responses[500] = { 
+        description: 'Internal server error while retrieving the author details' 
+     }
+  */
   const id = req.params.author_id;
   try {
     const data = await Authors.findById(id);
@@ -57,13 +87,16 @@ exports.findOne = async (req, res) => {
 };
 
 exports.create = async (req, res) => {
-  /* 
-    #swagger.tags = ['Authors']
-    #swagger.description = 'Endpoint to create a new author in the database.'
-    #swagger.parameters['body'] = {
-    in: 'body',
-    description: 'Author create data',
-    schema: {
+  /* #swagger.tags = ['Authors']
+     #swagger.summary = 'Create a new author'
+     #swagger.description = 'Creates a new author after validating their information.'
+     #swagger.security = [{ "github_auth": [] }]
+    
+     #swagger.parameters['body'] = {
+        in: 'body',
+        description: 'New author information',
+        required: true,
+        schema: {
         firstName: "John",
         lastName: "Doe",
         pseudonym: "John Doe",
@@ -73,7 +106,19 @@ exports.create = async (req, res) => {
         mainGenre: "Fiction",
         books: ["Book 1", "Book 2"]
     }
-} */
+    }
+
+    #swagger.responses[201] = { 
+        description: 'Author created successfully',
+        schema: { id: '65f9a1b2c3d4e5f6g7h8i9j0' }
+    }
+    #swagger.responses[400] = { 
+        description: 'Validation error in the provided data' 
+    }
+    #swagger.responses[500] = { 
+        description: 'Internal server error' 
+    }
+  */
   if (!req.body.firstName) {
     return res.status(400).send({
       message: 'Content can not be empty',
@@ -103,13 +148,23 @@ exports.create = async (req, res) => {
 };
 
 exports.update = async (req, res) => {
-  /*
-    #swagger.tags = ['Authors']
-    #swagger.description = 'Endpoint to update an existing author by its ID.'
-    #swagger.parameters['body'] = {
-    in: 'body',
-    description: 'Author update data',
-    schema: {
+  /* #swagger.tags = ['Authors']
+     #swagger.summary = 'Update an author'
+     #swagger.description = 'Updates an existing author by their ID.'
+     #swagger.security = [{ "github_auth": [] }]
+
+      #swagger.parameters['author_id'] = {
+        in: 'path',
+        description: 'The unique ID of the author to update',
+        required: true,
+        type: 'string'
+      }
+
+     #swagger.parameters['body'] = {
+        in: 'body',
+        description: 'Update user information',
+        required: true,
+        schema: {
         firstName: "John",
         lastName: "Doe",
         pseudonym: "John Doe",
@@ -119,7 +174,19 @@ exports.update = async (req, res) => {
         mainGenre: "Fiction",
         books: ["Book 1", "Book 2"]
     }
-} */
+    }
+
+    #swagger.responses[204] = { 
+        description: 'Author information updated successfully',
+        schema: { id: '65f9a1b2c3d4e5f6g7h8i9j0' }
+    }
+    #swagger.responses[400] = { 
+        description: 'Validation error in the provided data' 
+    }
+    #swagger.responses[500] = { 
+        description: 'Internal server error' 
+    }
+  */
   if (!req.body) {
     return res.status(400).send({
       message: 'Data to update can not be empty!',
@@ -144,9 +211,28 @@ exports.update = async (req, res) => {
 };
 
 exports.delete = async (req, res) => {
-  /*
-  #swagger.tags = ['Authors']
-  #swagger.description = 'Endpoint to delete an existing author by its ID.' */
+  /* #swagger.tags = ['Authors']
+     #swagger.summary = 'Delete an author'
+     #swagger.description = 'Permanently removes an author from the database using their unique ID.'
+     #swagger.security = [{ "github_auth": [] }]
+
+     #swagger.parameters['author_id'] = {
+        in: 'path',
+        description: 'The unique ID of the author to delete',
+        required: true,
+        type: 'string'
+     }
+
+     #swagger.responses[200] = { 
+        description: 'Author deleted successfully.' 
+     }
+     #swagger.responses[404] = { 
+        description: 'Author not found. No deletion was performed.' 
+     }
+     #swagger.responses[500] = { 
+        description: 'Internal server error while attempting to delete the author' 
+     }
+  */
   const id = req.params.author_id;
   try {
     const data = await Authors.findByIdAndDelete(id);
